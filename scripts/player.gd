2e081de823ab0d749707ec120167758e5c07e249
EnzoButtiero <94783966+EnzoButtiero@users.noreply.github.com>
2024-01-26 22:08:48 +0100

    add inputs and camera zoom

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,14 +3,25 @@ extends CharacterBody3D
 @onready var pivot = $Pivot
 @onready var camera = $Pivot/Camera3D
 
-@export var sens_speed = 5.0
+const SENS_SPEED = 3.0
+const LERP_ZOOM_SPEED = 9.0
+
+var zoom : bool = false
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _process(delta):
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	
-	rotate_y(deg_to_rad(-input_dir.x))
-	pivot.rotate_x(deg_to_rad(-input_dir.y))
-	pivot.rotation.x = clamp(pivot.rotation.x,deg_to_rad(-50),deg_to_rad(50))
+	if Input.is_action_just_pressed("zoom"):
+		zoom = !zoom
+	
+	if zoom:
+		camera.position.z = lerp(camera.position.z,-2.0,delta*LERP_ZOOM_SPEED)
+	else:
+		camera.position.z = lerp(camera.position.z,pivot.position.z,delta*LERP_ZOOM_SPEED)
+	
+	rotate_y(deg_to_rad(-input_dir.x*SENS_SPEED))
+	pivot.rotate_x(deg_to_rad(-input_dir.y*SENS_SPEED))
+	pivot.rotation.x = clamp(pivot.rotation.x,deg_to_rad(0),deg_to_rad(50))
 	move_and_slide()

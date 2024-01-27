335cf15f3fcc278c04343934faa49cffebdba9a2
EnzoButtiero <94783966+EnzoButtiero@users.noreply.github.com>
2024-01-27 15:55:12 +0100

    add main menu

---
+++ scripts/scene_switcher.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@export_file("*.tscn") var main_menu_scene
+@export_file("*.tscn") var start_game_scene
+
+func _on_pause_menu_main_menu_pressed():
+	if main_menu_scene:
+		get_tree().change_scene_to_file(main_menu_scene)
+	else:
+		print("No main menu scene set in the Scene switcher.")
+
+
+func _on_play_button_pressed():
+	if start_game_scene: 
+		get_tree().change_scene_to_file(start_game_scene)
+	else:
+		print("No start game scene set in the Scene switcher.")
